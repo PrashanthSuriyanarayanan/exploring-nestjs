7e41db0329b60f61e053908be4c9429ec13373a9
Prashanth <prashu.suri@gmail.com>
2025-08-09 21:50:00 +0530

    feat(*): adding logger

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TrimTrailingWhitespacesPipe } from './common/pipes/trimTrailingWhitespaces.pipe';
+import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
   app.useGlobalPipes(new TrimTrailingWhitespacesPipe());
+  app.useGlobalInterceptors(new LoggingInterceptor());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
