8fdde167538b3b0b5113c3c51d46a6aeed10dd43
Prashanth <prashu.suri@gmail.com>
2025-08-09 22:16:14 +0530

    feat(*): enable cors for a host

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ Controller > Service > DTO > Entity > Database
 
 Controller → validates with DTO → passes to Service → stores in Entity via TypeORM → DB.
 
+Host Mapping to test CORS
+echo 127.0.0.1 localhost nest-poc.com >> C:\Windows\System32\Drivers\etc\hosts
+
+echo ::1 localhost nest-poc.com >> C:\Windows\System32\Drivers\etc\hosts
--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,20 @@ import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
   app.useGlobalPipes(new TrimTrailingWhitespacesPipe());
+
   app.useGlobalInterceptors(new LoggingInterceptor());
+
+  // Yet to test the browser behavior
+  app.enableCors({
+    origin: ['http://nest-poc.com:3000'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true
+  })
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
+
